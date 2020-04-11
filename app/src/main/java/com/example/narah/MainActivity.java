5bfab11687ce3d2cb178ee4ee4e49bf847a1fb81
lenovo <saja1141906@hotmail.com>
2020-04-11 17:57:41 +0300

    test commit

--- app/src/main/java/com/example/narah/MainActivity.java
+++ app/src/main/java/com/example/narah/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.narah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+//this is test2S
+    private Button btg_onclick ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is test2S
+        btg_onclick=findViewById(R.id.btg_onclick);
+    }
+
+
+
+    public void btg(View view) {
+       // Intent intent= new Intent(this,class name);
+      //    startActivity(intent);
     }
 }
